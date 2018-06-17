abde653b06e942d208ad46ce136cd6500624b0d5
Tomasz Klim <github@tomaszklim.pl>
2018-06-17 22:41:06 +0200

    experimental megaraid support

---
+++ cron/update-megaraid.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# On some (older) servers, MegaRAID drives are not detected
+# by list-megaraid-drives.sh script, or in any other method.
+# The only way to monitor them is to add them manually to
+# crontab, eg.:
+#
+# /opt/farm/ext/monitoring-smart/handlers/megaraid.sh megaraid,0 /dev/sda
+
+
+exec 9>/var/run/smart-megaraid.lock
+if ! flock -n 9; then exit 0; fi
+
+handles=`/opt/farm/ext/storage-utils/list-megaraid-drives.sh`
+
+for handle in $handles; do
+	/opt/farm/ext/monitoring-smart/handlers/megaraid.sh $handle /dev/bus/0
+done
---
+++ handlers/megaraid.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+handle=$1
+node=$2
+
+path="/var/cache/cacti"
+
+file="$path/`echo $handle |tr ',' '-'`.tmp"
+/usr/sbin/smartctl -d $handle -i $node >$file
+
+if ! grep -q " SAS " $file; then
+	model=`grep 'Device Model:' $file |awk '{ print $3 }'`
+	serial=`grep 'Serial Number:' $file |awk '{ print $3 }'`
+
+	deviceid="${model}_${serial}"
+	file2="$path/ata-$deviceid.txt"
+
+	/usr/sbin/smartctl -d $handle -a $node >$file2.new
+
+	mv -f $file2.new $file2 2>/dev/null
+	/opt/farm/ext/monitoring-smart/targets/sata.sh $deviceid $file2
+fi
