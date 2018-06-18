e87ca30b0acf2e9867302e9465a0c80aaf74ac40
Tomasz Klim <github@tomaszklim.pl>
2018-06-18 11:58:18 +0200

    catch all device model name parts (for Corsair and Samsung SSD drives, with spaces in model name)

--- handlers/megaraid.sh
+++ handlers/megaraid.sh
@@ -9,7 +9,7 @@ file="$path/`echo $handle |tr ',' '-'`.tmp"
 /usr/sbin/smartctl -d $handle -i $node >$file
 
 if ! grep -q " SAS" $file; then
-	model=`grep 'Device Model:' $file |awk '{ print $3 }'`
+	model=`grep 'Device Model:' $file |awk '{ print $3 $4 $5 $6 $7 $8 $9 }'`
 	serial=`grep 'Serial Number:' $file |awk '{ print $3 }'`
 
 	deviceid="${model}_${serial}"
