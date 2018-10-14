15627f205fc6375c42eecb2e5387b1022f543621
Tomasz Klim <github@tomaszklim.pl>
2018-10-14 14:39:42 +0200

    parser for sas drives smart info

--- targets/sas.sh
+++ targets/sas.sh
@@ -3,6 +3,10 @@
 deviceid=$1
 file=$2
 
+if [ -d /opt/farm/ext/monitoring-heartbeat ]; then
+	/opt/farm/ext/monitoring-smart/targets/sas-heartbeat.sh $deviceid $file
+fi
+
 if [ -d /opt/farm/ext/monitoring-cacti ]; then
 	/opt/farm/ext/monitoring-cacti/cron/send.sh $file
 fi
